bfe0325d8a99e60a35ee2485bde85be271c06214
Xingjian-Bi <1569398343@qq.com>
2022-11-14 22:40:37 -0800

    added passport auth and backend part

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const session = require("express-session");
+//Used in authConfig.js
+// const session = require("express-session");
 const router = require("./routes/route.js");
+const configurePassport = require("./auth/authConfig.js");
 
 const port = process.env.PORT || 8000;
 const app = express();
 
 
-app.use(
-	session({
-		secret: "user name",
-		resave: false,
-		saveUninitialized: true,
-		cookie: { maxAge: 120000 },
-	})
-);
+// app.use(
+// 	session({
+// 		secret: "user name",
+// 		resave: false,
+// 		saveUninitialized: true,
+// 		cookie: { maxAge: 120000 },
+// 	})
+// );
+
 app.use(bodyParser.json());
 app.use(
 	bodyParser.urlencoded({
@@ -32,6 +35,9 @@ app.use(express.static(path.join(__dirname, "front/build")));
 // app.get("/", (req, res) => {
 // 	res.send("Hi");
 // });
+
+configurePassport(app);
+
 
 //How do we start listening to the server
 app.listen(port, () => console.log(`server started on port ${port}`));
