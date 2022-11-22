f820bd3ad62df3737d2b63d0ce00c621e778a9cd
Yi Zhou <92965215+HotdrynoodlesTauren@users.noreply.github.com>
2022-11-21 22:42:34 -0800

    frontend update

--- front/src/components/buildings/BuildingGrid.js
+++ front/src/components/buildings/BuildingGrid.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // import PropTypes from 'prop-types';
-import testImg from './test.jpeg';
 import BuildingUnit from '../buildings/BuildingUnit';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
+// function BuildingGrid({buildings}) {
 function BuildingGrid() {
+  const [buildings, setBuildings] = useState([]);
+
+  useEffect(() => {
+    const fetchBuildings = async () => {
+      const rawData = await fetch('/allarchitectures');
+      const res = await rawData.json();
+      setBuildings(res);
+    };
+    fetchBuildings();
+  }, []);
+
   return (
     <div>
       <div style={buildingStyle}>
-        {/* {BuildingUnit.map((building) => (
-          <Building key={user.id} user={user} />
-        ))} */}
+        {buildings.map((building) => (
+          <BuildingUnit key={building._id} building={building} />
+        ))}
       </div>
     </div>
   );
--- routes/route.js
+++ routes/route.js
@@ -1,105 +1,106 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const myDB = require("../db/myDB.js");
+const myDB = require('../db/myDB.js');
 // const passport = require("passport");
-require("dotenv").config();
+require('dotenv').config();
 
-router.post("/signin", async (req, res) => {
-	try{
-		const userRes = await myDB.authenticate(req.body.username, req.body.password);
-		if (userRes) {
-			req.session.user = req.body.username;
-			res.redirect("/buildings");
-		}else{
-			res.json({error: "User already exists"});
-		}
-
-	}catch (e) {
-		res.status(400).send({ err: e });
-	}
-
+router.post('/signin', async (req, res) => {
+  try {
+    const userRes = await myDB.authenticate(
+      req.body.username,
+      req.body.password
+    );
+    if (userRes) {
+      req.session.user = req.body.username;
+      res.redirect('/buildings');
+    } else {
+      res.json({ error: 'User already exists' });
+    }
+  } catch (e) {
+    res.status(400).send({ err: e });
+  }
 });
 
-router.get("/logout", (req, res) => {
-	req.logout();
-	res.redirect("/signin");
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/signin');
 });
 
 // route for registering a new user
-router.post("/registerUser", async  (req, res) => {
-	try {
-		const findUserRes = await myDB.findUserName(req.body.userName);
-		// console.log("Get username from login", findUserRes);
+router.post('/registerUser', async (req, res) => {
+  try {
+    const findUserRes = await myDB.findUserName(req.body.userName);
+    // console.log("Get username from login", findUserRes);
 
-		// If findUserRes array is empty then we call registerUser function
-		if (findUserRes) {
-			res.json({error: "User already exists"});
-		}
-		const registerUserRes = await myDB.registerUser(
-			req.body.username,
-			req.body.password
-		);
-		console.log("Created user in db", registerUserRes);	
-		res.redirect("/login");
-		// Send findUserRes to frontend and it will update accordingly
-		// res.send({ users: findUserRes });
-
-	} catch (e) {
-		res.status(400).send({ err: e });
-	}
+    // If findUserRes array is empty then we call registerUser function
+    if (findUserRes) {
+      res.json({ error: 'User already exists' });
+    }
+    const registerUserRes = await myDB.registerUser(
+      req.body.username,
+      req.body.password
+    );
+    console.log('Created user in db', registerUserRes);
+    res.redirect('/login');
+    // Send findUserRes to frontend and it will update accordingly
+    // res.send({ users: findUserRes });
+  } catch (e) {
+    res.status(400).send({ err: e });
+  }
 });
 
 //route for get all architectures
-router.get("/architectures/:findBy/:value", async (req, res) => {
-	try{
-		const archiRes = await myDB.getArchitectures(req.params.findBy, req.params.value);
-		console.log("get architectures data from db ", archiRes);
-		res.send({archiRes});
-	}catch(e){
-		res.status(400).send({ err: "error-route" });
-	}
+router.get('/architectures/:findBy/:value', async (req, res) => {
+  try {
+    const archiRes = await myDB.getArchitectures(
+      req.params.findBy,
+      req.params.value
+    );
+    console.log('get architectures data from db ', archiRes);
+    res.send(archiRes);
+  } catch (e) {
+    res.status(400).send({ err: 'error-route' });
+  }
 });
 
-router.get("/allarchitectures", async (req, res) => {
-	try{
-		const archiRes = await myDB.getAllArchitectures();
-		console.log("get all architectures data from db ", archiRes);
-		res.send({archiRes});
-	}catch(e){
-		res.status(400).send({ err: e });
-	}
+router.get('/allarchitectures', async (req, res) => {
+  try {
+    const archiRes = await myDB.getAllArchitectures();
+    console.log('get all architectures data from db ', archiRes);
+    res.send(archiRes);
+  } catch (e) {
+    res.status(400).send({ err: e });
+  }
 });
 
-router.post("/myItinerary", async (req, res) => {
-	try{
-		const itineraryRes = await myDB.addItinerary(req.body);
-		console.log("added an itinerary from db ", itineraryRes);
-		res.send({status : "ok"});
-	}catch(e){
-		res.status(400).send({ err: e });
-	}
+router.post('/myItinerary', async (req, res) => {
+  try {
+    const itineraryRes = await myDB.addItinerary(req.body);
+    console.log('added an itinerary from db ', itineraryRes);
+    res.send({ status: 'ok' });
+  } catch (e) {
+    res.status(400).send({ err: e });
+  }
 });
 
-
-router.post("/addStop", async (req, res) => {
-	try{
-		const addRes = await myDB.addStop(req.body.itinerayID, req.body.archiID);
-		console.log("add stop in itineray from db ", addRes);
-		res.send({status : "ok"});
-	}catch(e){
-		res.status(400).send({ err: e });
-	}
+router.post('/addStop', async (req, res) => {
+  try {
+    const addRes = await myDB.addStop(req.body.itinerayID, req.body.archiID);
+    console.log('add stop in itineray from db ', addRes);
+    res.send({ status: 'ok' });
+  } catch (e) {
+    res.status(400).send({ err: e });
+  }
 });
 
-router.post("/deleteStop", async (req, res) => {
-	try{
-		const delRes = await myDB.deleteStop(req.body.archiID);
-		console.log("deleted stop in itineray from db ", delRes);
-		res.send({status : "ok"});
-	}catch(e){
-		res.status(400).send({ err: e });
-	}
+router.post('/deleteStop', async (req, res) => {
+  try {
+    const delRes = await myDB.deleteStop(req.body.archiID);
+    console.log('deleted stop in itineray from db ', delRes);
+    res.send({ status: 'ok' });
+  } catch (e) {
+    res.status(400).send({ err: e });
+  }
 });
-
 
 module.exports = router;
